3784fd71924d08a4d889bb76ceeab7934d9ee5ad
Greeyouate2 <118512147+Greeyouate2@users.noreply.github.com>
2023-09-20 12:52:33 +0900

    Update chat.php

--- chat.php
+++ chat.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
-  <title>チャット</title>
+  <title>ChatRooms</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/fontawesome-free-5.15.3-web/css/all.min.css">
   <script src="js/main.js"></script>
@@ -15,6 +15,7 @@
         <textarea id="textarea" type="text" name="text" rows="1" required placeholder="message.."></textarea>
         <input type="submit" name="submit" value="送信" id="search">
         <label for="search"><i class="far fa-paper-plane"></i></label>
+        <input id="web-chat"><label for="web-chat">  <i class="far fa-paper-plane" style="cursor:pointer;"></i></label>
       </form>
     </div>
   </main>
